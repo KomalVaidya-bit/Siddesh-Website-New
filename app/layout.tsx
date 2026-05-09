25ffd13e34a811c940e7db5ba53ddd0cd962a844
Komal Vaidya <komalvaidya1414@gmail.com>
2026-05-09 23:11:58 +0530

    navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,41 +1,53 @@
-import type { Metadata } from "next";
-import { Sora } from "next/font/google";
-import type { ReactNode } from "react";
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
-import "./globals.css";
+import type { Metadata } from "next"
+import { Sora } from "next/font/google"
+import type { ReactNode } from "react"
+
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
+
+import "./globals.css"
 
 import { CartProvider } from "@/context/CartContext"
-
-
-
-
 
 const sora = Sora({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
-});
+})
 
 export const metadata: Metadata = {
   title: "Siddesh Website",
-  description: "Cinematic table scroll animation",
-};
+  description: "Professional Ecommerce Website",
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: ReactNode;
+  children: ReactNode
 }>) {
+
   return (
+
     <html lang="en">
-      <body>
-  <CartProvider>
-    {children}
-  </CartProvider>
-</body>
+
+      <body className={sora.className}>
+
+        <CartProvider>
+
+          {/* NAVBAR */}
+          <Navbar />
+
+          {/* PAGE CONTENT */}
+          <main>
+            {children}
+          </main>
+
+          {/* FOOTER */}
+          <Footer />
+
+        </CartProvider>
+
+      </body>
+
     </html>
-  );
+  )
 }
-
-
-
